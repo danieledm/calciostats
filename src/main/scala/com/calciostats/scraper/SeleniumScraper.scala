9e053aad2108348bc19d894cde34ddf8542c4d99
danieledm <daniele.dallemule@yahoo.com>
2017-01-15 00:41:26 +0100

    collect statistics

--- src/main/scala/com/calciostats/scraper/SeleniumScraper.scala
+++ src/main/scala/com/calciostats/scraper/SeleniumScraper.scala
@@ -19,19 +19,27 @@ object BlogSpec  extends App with WebBrowser {
 
   val eles: Iterator[_root_.com.calciostats.scraper.BlogSpec.Element] = findAll(className("ws-list"))
 
-  eles.map(el => findChildElementByName(el.underlying, "td").map(toText)).foreach(println)
+  val statsList = eles.flatMap(el => findChildElementByName(el.underlying, "td").map(toText)).toList
 
+  val values = statsList.zipWithIndex.filter(e => (e._2 + 1) % 2 == 0).map(_._1)
+  val keys = statsList.zipWithIndex.filter(e => (e._2 + 1) % 2 == 1).map(_._1)
+
+  val keyValueStats = keys.zip(values).foreach(e => println(e._1 + " = " + e._2))
 
 
   def toText(element: WebElement): String = {
-//    findChildElementByName(element, "span").map(_.getText).mkString(" - ")
-    element.getText
+    val spanElements = findChildElementByName(element, "span")
+    if (spanElements.length == 3) {
+      spanElements.tail.map(_.getText).mkString(" - ")
+    } else {
+      element.getText
+    }
   }
 
 
   def findChildElementByName(element: WebElement, name: String): List[WebElement] = {
     import scala.collection.JavaConversions._
-    element.findElements(org.openqa.selenium.By.tagName("td")).toList
+    element.findElements(org.openqa.selenium.By.tagName(name)).toList
   }
 
 
