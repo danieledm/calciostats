98d12fc45464a91c7da77dd1506bad10b9a62dde
danieledm <daniele.dallemule@yahoo.com>
2017-01-15 02:13:03 +0100

    almost complete

--- src/main/scala/com/calciostats/scraper/SeleniumScraper.scala
+++ src/main/scala/com/calciostats/scraper/SeleniumScraper.scala
@@ -17,30 +17,69 @@ object BlogSpec  extends App with WebBrowser {
 
   val title = pageTitle
 
-  val eles: Iterator[_root_.com.calciostats.scraper.BlogSpec.Element] = findAll(className("ws-list"))
 
-  val statsList = eles.flatMap(el => findChildElementByName(el.underlying, "td").map(toText)).toList
+//  getSituationStatistics("team-goals-content").foreach(println)
+//  getSituationStatistics("team-passes-content").foreach(println)
+//  getSituationStatistics("team-cards-content").foreach(println)
+//  collectGeneralStatistics().foreach(println)
 
-  val values = statsList.zipWithIndex.filter(e => (e._2 + 1) % 2 == 0).map(_._1)
-  val keys = statsList.zipWithIndex.filter(e => (e._2 + 1) % 2 == 1).map(_._1)
+  go to "https://www.whoscored.com/stagestatfeed/?against=0&field=0&stageId=14014&teamId=75&type=6"
 
-  val keyValueStats = keys.zip(values).foreach(e => println(e._1 + " = " + e._2))
+  val body = pageSource
+  println(body)
 
+  close()
 
-  def toText(element: WebElement): String = {
-    val spanElements = findChildElementByName(element, "span")
-    if (spanElements.length == 3) {
-      spanElements.tail.map(_.getText).mkString(" - ")
-    } else {
-      element.getText
-    }
+  def getSituationStatistics(elementId: String ): List[(String, String)] = {
+    val table: List[_root_.com.calciostats.scraper.BlogSpec.Element] = findAll(id(elementId)).toList
+
+    val situationsStats = Utils.findChildElementByName(table.head.underlying, "tr")
+      .map(Utils.findChildElementByName(_, "td").toList)
+      .filter(_.size == 3)
+      .flatMap(x => List((x.head.getText, x(1).getText), (x.head.getText + " %", x(2).getText)))
+
+    situationsStats
   }
 
 
+  def collectGeneralStatistics(): List[(String, String)] = {
+
+    def toText(element: WebElement): String = {
+      val spanElements = Utils.findChildElementByName(element, "span")
+      if (spanElements.length == 3) {
+        spanElements.tail.map(_.getText).mkString(" - ")
+      } else {
+        element.getText
+      }
+    }
+
+    val eles: Iterator[_root_.com.calciostats.scraper.BlogSpec.Element] = findAll(className("ws-list"))
+
+    val statsList = eles.flatMap(el => Utils.findChildElementByName(el.underlying, "td").map(toText)).toList
+
+    val values = statsList.zipWithIndex.filter(e => (e._2 + 1) % 2 == 0).map(_._1)
+    val keys = statsList.zipWithIndex.filter(e => (e._2 + 1) % 2 == 1).map(_._1)
+
+    keys.zip(values)
+  }
+
+
+}
+
+
+object Utils {
   def findChildElementByName(element: WebElement, name: String): List[WebElement] = {
     import scala.collection.JavaConversions._
     element.findElements(org.openqa.selenium.By.tagName(name)).toList
   }
 
+  def findChildElementByXPath(element: WebElement, xpath: String): List[WebElement] = {
+    import scala.collection.JavaConversions._
+    element.findElements(org.openqa.selenium.By.xpath(xpath)).toList
+  }
 
+  def findChildElementById(element: WebElement, id: String): List[WebElement] = {
+    import scala.collection.JavaConversions._
+    element.findElements(org.openqa.selenium.By.id(id)).toList
+  }
 }
